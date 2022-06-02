46214472b0a99c6961d853caaeb7c9aa37e07298
samueleffa <seffa1@gmail.com>
2022-06-02 05:55:31 -0400

    asdf

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 // https://www.w3schools.com/html/html5_canvas.asp
+"use strict";
+import Paddle from "./paddle.js";
 
 let canvas = document.getElementById("gameScreen");
 
 let context = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
 context.clearRect(0, 0, 800, 600);
 
 context.fillStyle = "red";
 context.fillRect(20, 20, 100, 100);
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+// paddle.draw();
+
+let a = 5;
+console.log(a);
---
+++ src/paddle.js
@@ -0,0 +1,14 @@
+export default class Paddle {
+  constructor(GAME_WIDTH, GAME_HEIGHT) {
+    this.width = 150;
+    this.height = 30;
+    this.position = {
+      x: GAME_WIDTH / 2 - this.width / 2,
+      y: GAME_HEIGHT - this.height - 10,
+    };
+  }
+
+  //   draw(context) {
+  //     context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  //   }
+}
